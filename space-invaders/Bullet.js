c13b96d8d667d8490fa4a95942b2799cf347a35c
David "Cou <100618303+cou201@users.noreply.github.com>
2025-01-27 19:58:54 -0500

    Update Bullet.js

--- space-invaders/Bullet.js
+++ space-invaders/Bullet.js
@@ -2,7 +2,7 @@ export default class Bullet {
   constructor(canvas, x, y, velocity, bulletColor) {
     this.canvas = canvas;
     this.x = x;
-    this.y = y;
+    this.y = y
     this.velocity = velocity;
     this.bulletColor = bulletColor;
 
